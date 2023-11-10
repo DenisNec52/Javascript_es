998f683b722f321dcbd81fd43fd000c2ae74f21d
DenisNec52 <148898216+DenisNec52@users.noreply.github.com>
2023-11-10 21:05:52 +0100

    Add files via upload

---
+++ Javascript_es/index.js
@@ -0,0 +1,46 @@
+//somma Es 2
+{
+    console.log(12+20);
+}
+// Es 3
+{
+    var somma = 32;
+var x = 12;
+console.log(x);
+console.log(somma);
+}
+// Es 4
+{
+var name = "Necula Denis Alexandru"
+console.log(name);}
+//Es 5
+{
+var z = 4;
+var sottrazione = (z-x);
+console.log(sottrazione);
+}
+//Es 6
+let car_driver = {
+	name: "Denis Alexandru",
+    surname: "Necula",
+    age: 19,
+    car: "Golf",
+};
+console.log(car_driver);
+//Es 7
+{
+var name1 = "john";
+var name2 = "John";
+console.log(name1 == name2); // false
+}
+{
+let result = name2.toLowerCase();
+console.log(result);
+console.log(name1 == result)
+}
+// Uguaglianza stretta
+// console.log(52 === 52); // true
+// Disuguaglianza debole
+// console.log(52 != '52'); // false
+// Disuguaglianza stretta
+// console.log(52 !== 52); // false
